0e3d5cb0210e9a68436cca26c37c77cac00df729
Rizwanullah-warr <rizwanullahwarr02@gmail.com>
2026-01-29 08:50:13 +0500

    dir

--- week4/index.js
+++ week4/index.js
@@ -4,4 +4,4 @@
 
 const path=require("path");
 console.log(__dirname);
-console.log(__dirname + "/e:100x/devsweek4/index.j
+console.log(__dirname + "/e:100x/devsweek4/index.js")
--- week4/index.mjs
+++ week4/index.mjs
@@ -1,8 +1,7 @@
-// import chalk from "chalk";
+ import chalk from "chalk";
 
 
-
-// console.log(chalk.blue('Hello, world!'));
+ console.log(chalk.blue('Hello, world!'));
 // console.log(chalk.red.bold('This is an error message.'));
 // console.log(chalk.green.underline('This is a success message.'));
 
