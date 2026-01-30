c23a9b634310cbe544863a4a73fd4cb0921d2e27
Rizwanullah-warr <rizwanullahwarr02@gmail.com>
2026-01-30 10:58:44 +0500

    app js

--- week4/index.js
+++ week4/index.js
@@ -3,5 +3,6 @@
 // console.log(a+b);
 
 const path=require("path");
-console.log(__dirname);
-console.log(__dirname + "/e:100x/devsweek4/index.js")
+//console.log(__dirname);
+//console.log(__dirname + "/e:100x/devsweek4/index.js");
+console.log(path.join(__dirname,"../../100x/devsweek4","index.js"));
